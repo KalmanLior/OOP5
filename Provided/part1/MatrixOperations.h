fed4b22e8d4a8580e553552d8c9a083f6469a6cb
khturekni <lior.kalman@outlook.com>
2019-01-17 23:06:39 +0200

    implemented Utilities.h

--- Provided/part1/MatrixOperations.h
+++ Provided/part1/MatrixOperations.h
@@ -3,4 +3,8 @@
 #ifndef OOP5_MATRIXOPERATIONS_H
 #define OOP5_MATRIXOPERATIONS_H
 
+template<typename... E>
+struct Add{};
+
+
 #endif //OOP5_MATRIXOPERATIONS_H
--- Provided/part1/Utilities.h
+++ Provided/part1/Utilities.h
@@ -3,41 +3,89 @@
 
 template<int N>
 struct Int{
-    int value = N;
+    constexpr static int value = N;
+};
+
+// a template for unpacking a list
+template<typename H, typename... Elements>
+struct UnpackedList{
+    typedef H head;
+    // this does the "unpacking"
+    typedef UnpackedList<Elements...> next;
+    constexpr static int size = sizeof...(Elements) + 1;
+};
+
+template<typename H>
+struct UnpackedList<H>{
+    typedef H head;
+    constexpr static int size = 1;
 };
 
 
-template<typename Head, typename... Elements>
+//======================================= List and specializations =======================================//
+template<typename... Elements>
 struct List{
-    Head head;
-    List<Elements...> next;
-    int size;
+private:
+    typedef UnpackedList<Elements...> unpacked_list;
+public:
+    typedef typename unpacked_list::head head;
+    // this does the "unpacking"
+    typedef typename unpacked_list::next next;
+    constexpr static int size = sizeof...(Elements);
 };
 
-template<int N>
-struct List<Int<N>>{
-    int head = N;
-    int size = 1;
-};
-
-template<typename T, List<T> L>
-struct PrependList{
-    List<T> list = L;
-};
-
-template<int N, List<T> L>
-struct ListGet{
-    T head;
-    List<T> next;
-    int size;
-};
-
+//specialization - a list with 1 element, this is our terminating condition
 template<typename T>
-struct ListSet{
-    T head;
-    List<T> next;
-    int size;
+struct List<T>{
+    typedef T head;
+    typedef List next;
+    constexpr static int size = 1;
 };
 
+//specialization - a list with no elements, this is our terminating condition
+template<>
+struct List<>{
+    constexpr static int size = 0;
+};
+
+
+//======================================= PrependList and specializations =======================================//
+template<typename T, typename... N>
+struct PrependList{
+    typedef List<T, N...> list;
+};
+
+//======================================= ListGet and specializations =======================================//
+template<int N, typename T>
+struct ListGet{};
+
+template<int N, typename T, typename... L>
+struct ListGet<N, List<T, L...>>{
+    typedef typename ListGet<N-1, List<L...>>::value value;
+
+};
+
+template<typename T, typename... L>
+struct ListGet<0, List<T, L...>>{
+        typedef T value;
+};
+
+
+
+//======================================= ListSet and specializations =======================================//
+template<int N, typename T, typename... L>
+struct ListSet{};
+
+template<int N, typename T, typename... L>
+struct ListSet<N, T, List<L...>>{
+    typedef typename PrependList<typename List<L...>::head,
+                                 typename ListSet<N-1, T, typename List<L...>::next>::list>
+                                 ::list list;
+};
+
+template<typename T, typename... L>
+struct ListSet<0, T, List<L...>>{
+    typedef typename PrependList<T, typename List<L...>::next>::list list;
+};
 
 #endif //OOP5_PART1_UTILITIES_H
