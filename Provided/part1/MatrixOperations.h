251e1e03e873552e6267dd4ae39e07bf92e641cb
YoniBenZvi <44531536+YoniBenZvi@users.noreply.github.com>
2019-01-19 16:36:40 +0200

    Add files via upload

--- Provided/part1/MatrixOperations.h
+++ Provided/part1/MatrixOperations.h
@@ -1,10 +1,69 @@
-#include "Transpose.h"
-
 #ifndef OOP5_MATRIXOPERATIONS_H
 #define OOP5_MATRIXOPERATIONS_H
 
-template<typename... E>
-struct Add{};
+#include "Transpose.h"
 
+
+//============================================ Auxiliary ============================================//
+template<int i, int j, typename M>
+struct MatrixGet {
+    typedef typename ListGet<j, ListGet<i, M>::value>::value value;
+};
+
+template<int i, int j, typename T, typename M>
+struct MatrixSet {
+private:
+    typedef typename ListSet<j, T, ListGet<i, M>::value>::list newRow;
+public:
+    typedef typename ListSet<i, newRow, M>::list matrix;
+};
+
+template<typename M>
+struct MatrixSizes {
+    constexpr static int rows = M::size;
+    constexpr static int columns = ListGet<0, M>::value::size;
+};
+
+template<typename M1, typename M2>
+struct SizesCheck {
+    constexpr static bool addCond = (MatrixSizes<M1>::rows == MatrixSizes<M2>::rows) &&
+                                    ((MatrixSizes<M1>::columns == MatrixSizes<M2>::columns));
+    constexpr static bool mulCond = MatrixSizes<M1>::columns == MatrixSizes<M2>::rows;
+};
+
+//============================================ Add ============================================//
+template<int i, int j, typename M1, typename M2>
+struct AddHelper {
+private:
+    constexpr static int newInt = MatrixGet<i, j, M1>::value + MatrixGet<i, j, M2>::value;
+    typedef typename MatrixSet<i, j, Int < newInt>, M1>::matrix newMatrix;
+public:
+    typedef typename AddHelper<i, j - 1, newMatrix, M2>::result result;
+};
+
+template<int i, typename M1, typename M2>
+struct AddHelper<i, 0, M1, M2> {
+private:
+    constexpr static int newInt = MatrixGet<i, 0, M1>::value + MatrixGet<i, 0, M2>::value;
+    typedef typename MatrixSet<i, 0, Int < newInt>,M1>::matrix newMatrix;
+public:
+    typedef typename AddHelper<i - 1, MatrixSizes<M1>::columns - 1, newMatrix, M2>::result result;
+};
+
+template<typename M1, typename M2>
+struct AddHelper<0, 0, M1, M2> {
+private:
+    constexpr static int newInt = MatrixGet<0, 0, M1>::value + MatrixGet<0, 0, M2>::value;
+public:
+    typedef typename MatrixSet<0, 0, Int < newInt>,M1>::matrix result;
+};
+
+template<typename M1, typename M2>
+struct Add {
+    static_assert(SizesCheck<M1, M2>::addCond, "can't Add, sizes don't match\n");
+    typedef typename AddHelper<MatrixSizes<M1>::rows - 1, MatrixSizes<M1>::columns - 1, M1, M2>::result result;
+};
+
+//============================================ Multuply ============================================//
 
 #endif //OOP5_MATRIXOPERATIONS_H
--- Provided/part1/Utilities.h
+++ Provided/part1/Utilities.h
@@ -1,69 +1,70 @@
 #ifndef OOP5_PART1_UTILITIES_H
 #define OOP5_PART1_UTILITIES_H
 
+
+//============================================ Int ====================================//
 template<int N>
-struct Int{
+struct Int {
     constexpr static int value = N;
 };
 
-//======================================= List and specializations =======================================//
-template<typename... Elements>
-struct List{};
+//======================================= List =======================================//
+template<typename>
+struct List {
+};
 
-template<typename H,typename... Elements>
-struct List<H, Elements...>{
+template<typename H, typename... Elements>
+struct List<H, Elements...> {
 public:
     typedef H head;
-    // this does the "unpacking"
     typedef List<Elements...> next;
     constexpr static int size = sizeof...(Elements) + 1;
 };
 
-//specialization - a list with no elements, this is our terminating condition
 template<>
-struct List<>{
+struct List<> {
     constexpr static int size = 0;
 };
 
-//======================================= PrependList and specializations =======================================//
+//==================================== PrependList ====================================//
 template<typename T, typename... N>
-struct PrependList{};
+struct PrependList {
+};
 
 template<typename T, typename... N>
-struct PrependList<T, List<N...>>{
+struct PrependList<T, List<N...>> {
     typedef List<T, N...> list;
 };
 
-//======================================= ListGet and specializations =======================================//
-template<int N, typename T>
-struct ListGet{};
+//======================================= ListGet =======================================//
+template<int, typename>
+struct ListGet {
+};
 
 template<int N, typename T, typename... L>
-struct ListGet<N, List<T, L...>>{
-    typedef typename ListGet<N-1, List<L...>>::value value;
+struct ListGet<N, List<T, L...>> {
+    typedef typename ListGet<N - 1, List<L...>>::value value;
 };
 
 template<typename T, typename... L>
-struct ListGet<0, List<T, L...>>{
-        typedef T value;
+struct ListGet<0, List<T, L...>> {
+    typedef T value;
 };
 
-
-
-//======================================= ListSet and specializations =======================================//
+//======================================= ListSet =======================================//
 template<int N, typename T, typename... L>
-struct ListSet{};
+struct ListSet {
+};
 
 template<int N, typename T, typename... L>
-struct ListSet<N, T, List<L...>>{
-    typedef typename PrependList<typename List<L...>::head,
-                                 typename ListSet<N-1, T, typename List<L...>::next>::list>
-                                 ::list list;
+struct ListSet<N, T, List<L...>> {
+    typedef typename PrependList<List<L...>::head, ListSet<N - 1, T, List<L...>::next>::list>
+    ::list list;
 };
 
 template<typename T, typename... L>
-struct ListSet<0, T, List<L...>>{
-    typedef typename PrependList<T, typename List<L...>::next>::list list;
+struct ListSet<0, T, List<L...>> {
+    typedef typename PrependList<T, List<L...>::next>::list list;
 };
 
 #endif //OOP5_PART1_UTILITIES_H
